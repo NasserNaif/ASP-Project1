0666fb533578a7179f0b351446ceb908c0b49ea2
TETCO\naotaibi <naotaibi@tetco.sa>
2025-12-02 16:22:44 +0300

    apply Repository layer on Controller

--- Project1/Repository/IRegionRepo.cs
+++ Project1/Repository/IRegionRepo.cs
@@ -1,9 +1,18 @@
-﻿using Project1.Models.Domain;
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Models.Domain;
 
 namespace Project1.Repository
 {
     public interface IRegionRepo
     {
         public Task<List<Region>> GetAllSync();
+        public Task<Region> GetRegionById(Guid id);
+
+        public Task<Region> Add(Region region);
+
+        public Task<Region> Update(Guid id,Region region);
+
+        public Task<Region> Delete(Guid id);
+
     }
 }
--- Project1/Repository/RegionImpl.cs
+++ Project1/Repository/RegionImpl.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using Project1.Data;
 using Project1.Models.Domain;
 
@@ -13,9 +14,44 @@ namespace Project1.Repository
 
             _context = context;
         }
+
+        public async Task<Region> Add(Region region)
+        {
+            await _context.AddAsync(region);
+            _context.SaveChanges();
+
+            return region;
+        }
+
+        public async Task<Region> Delete(Guid id)
+        {
+            var existingRegion = await _context.Regions.FirstOrDefaultAsync(region => region.Id == id);
+
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            _context.Regions.Remove(existingRegion);
+            await _context.SaveChangesAsync();
+
+            return existingRegion;
+
+        }
+
         public async Task<List<Region>> GetAllSync()
         {
             return await _context.Regions.ToListAsync();
+        }
+
+        public Task<Region> GetRegionById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region> Update(Guid id, Region region)
+        {
+            throw new NotImplementedException();
         }
     }
 }
